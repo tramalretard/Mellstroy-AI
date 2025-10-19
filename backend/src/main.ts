929d1c5f1700649158844197b0435b048ea10f79
tramalretard <iknowozey@gmail.com>
2025-10-20 05:11:21 +0700

    backend: configure app.module and prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
+
+import { AppModule } from './app.module'
+import { getCorsConfig } from './configs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService)
+	const logger = new Logger(AppModule.name)
+
+	const host = config.getOrThrow<string>('SERVER_HOST')
+	const port = config.getOrThrow<number>('SERVER_PORT')
+
+	app.enableCors(getCorsConfig(config))
+	app.useGlobalPipes(new ValidationPipe())
+	app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+
+	try {
+		await app.listen(4000)
+
+		logger.log(`Сервер успешно запущен на: ${host}`)
+	} catch (error) {
+		logger.error(
+			`Не удалось запустить сервер на порту: ${port} ${error.message}`,
+			error
+		)
+		process.exit(1)
+	}
 }
-bootstrap();
+bootstrap()
